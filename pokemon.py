63d06e346396d829ad60751a83a6da4e196e3205
Felipe Guimaraes <contato.felipeguimaraes@gmail.com>
2021-12-07 20:52:51 -0300

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,32 +1,59 @@
 import requests
 import json
 
-from requests import api
 
 
 class pokemonClass:
-    def __init__(self, nome, habilidades, peso):
+
+    def __init__(self, url, nome, habilidades, tipo, peso):
         self.nome = nome
+        self.url = url
         self.habilidades = habilidades
         self.peso = peso
+        self.tipo = tipo
+
+    def __str__(self):
+
+      return  f'Nome:{self.nome} - url: {self.url} - tipo:{self.tipo} - Habilidades: {self.habilidades} - Peso: {self.peso}'  
+
+
+
+def url_pokemon(url: str) -> object:
+      pokemon = requests.get(url)
+
+      return pokemon   
 
 
 def pegar_pokemon():
     pokeList = requests.get('https://pokeapi.co/api/v2/pokemon/')
 
-    resultadospokemons = pokeList.json()['results']
-    listafiltradapokemons = []
+    resultadosPokemons = pokeList.json()['results']
+    
+    pokemonList = []
+    for pokemon in resultadosPokemons:
+        url = pokemon['url']
+        nome = pokemon['name']
+        pokeobjeto =  url_pokemon(pokemon['url']).json()
+        habilidades = [i['ability']['name'] for i in pokeobjeto['abilities']] 
+        peso = pokeobjeto['weight']
+        tipo = [i['type']['name'] for i in pokeobjeto['types']]
 
-    for pokemon in resultadospokemons:
-        listafiltradapokemons.append(pokemon)
-
-    for pokeobjeto in listafiltradapokemons:
-        pokeresulto = requests.get(pokeobjeto['url'])
-        pokemon = pokemonClass(pokeresulto.json()['species']['name'], pokeresulto.json()[
-                               'abilities'], pokeresulto.json()['weight'])
-
-        print(
-            f'Nome: {pokemon.nome} -  Habilidades: {pokemon.habilidades} - Peso: {pokemon.peso} - ')
+        pokemonList.append(pokemonClass(url,nome,habilidades,tipo,peso))
 
 
-pegar_pokemon()
+    return pokemonList
+
+
+
+pokemonObjeto = pegar_pokemon()
+
+for pokedex in pokemonObjeto:
+  print(pokedex)
+
+
+
+
+
+
+
+
